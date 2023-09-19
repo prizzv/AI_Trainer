6ae1763700f4b2c640a5e6c73a5743828c995ff6
Shashank <srkota1951@gmail.com>
2023-09-19 11:41:23 +0530

    completed deadlift model

---
+++ ML/videoSticher.py
@@ -0,0 +1,28 @@
+from moviepy.editor import *
+
+# TODO: Not working 
+def concatenate(video_clip_paths, output_path, method="compose"):
+    """Concatenates several video files into one video file
+    and save it to `output_path`. Note that extension (mp4, etc.) must be added to `output_path`
+    `method` can be either 'compose' or 'reduce':
+        `reduce`: Reduce the quality of the video to the lowest quality on the list of `video_clip_paths`.
+        `compose`: type help(concatenate_videoclips) for the info"""
+    # create VideoFileClip object for each video file
+    clips = [VideoFileClip(c) for c in video_clip_paths]
+    if method == "reduce":
+        # calculate minimum width & height across all clips
+        min_height = min([c.h for c in clips])
+        min_width = min([c.w for c in clips])
+        # resize the videos to the minimum
+        clips = [c.resize(newsize=(min_width, min_height)) for c in clips]
+        # concatenate the final video
+        final_clip = concatenate_videoclips(clips)
+    elif method == "compose":
+        # concatenate the final video with the compose method provided by moviepy
+        final_clip = concatenate_videoclips(clips, method="compose")
+    # write the output video file
+    final_clip.write_videofile(output_path)
+    final_clip.close()
+
+# concatenate("/dataset/deadlift", "singleDeadlift.mp4", method="compose")
+# print("Done!")
