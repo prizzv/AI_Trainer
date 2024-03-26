2b03d9b0c2f6a1f97829e9823fb6ab314bc64012
Shashank <srkota1951@gmail.com>
2024-03-26 12:59:09 +0530

    changed the config.py

--- Backend/flask/config.py
+++ Backend/flask/config.py
@@ -5,3 +5,23 @@ from dotenv import load_dotenv
 load_dotenv()
 
 FLASK_DEBUG = os.environ.get("FLASK_DEBUG", False)
+
+#NOTE: The above can also be done but the below allows for more configuration options
+# currently still using the above method
+
+# class Config(object):
+#     FLASK_DEBUG = False
+#     TESTING = False
+
+#     SECRET_KEY= 'randomsecret'
+
+# class ProductionConfig(Config):
+#     FLASK_DEBUG = False
+
+# class DevelopemntConfig(Config):
+#     FLASK_DEBUG = True
+#     TESTING = True
+
+# class TestingConfig(Config):
+#     FLASK_DEBUG = True
+#     TESTING = True
